241ece89d3cf86a6c901162bafcda155a65b3969
Harish S <harish@everest.engineering>
2022-03-01 16:28:01 +0530

    Add Testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { TextField } from "@mui/material";
+import { TextField as TextInput } from "@material-ui/core"
+import PhoneInput from "phone-number-material-ui";
+import MuiPhoneInput from "react-phone-input-material-ui";
+import "react-phone-input-material-ui/lib/style.css";
+import { useState } from "react";
 
 function App() {
+  const [phone, setPhone] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PhoneInput
+        value={phone}
+        onChange={(phone) => setPhone(phone)}
+        inputProps={{ variant: "standard" }}
+        component={TextField}
+        defaultConutry={"us"}
+      />
+      <div style={{ width: 300, paddingTop: 30 }}>
+        <MuiPhoneInput
+          name={"aasds"}
+          disabled={false}
+          value={phone}
+          component={TextField}
+          country="it"
+          inputProps={{ variant: "filled", label: "Phone", required: true, fullWidth: true }}
+          onChange={(val) => setPhone(val)}
+          onBlur={()=>{}}
+          // onKeyDown={e => e.key === 'Enter' && onBlur(e)}
+          defaultErrorMessage={"error"}
+          helperText={"helper text"}
+        />
+      </div>
+      <div style={{ width: 300, paddingTop: 30 }}>
+        <MuiPhoneInput
+          name={"aasds"}
+          disabled={false}
+          value={phone}
+          component={TextInput}
+          inputProps={{ variant: "filled", label: "Phone", required: true, fullWidth: true }}
+          onChange={(val) => setPhone(val)}
+          onBlur={()=>{}}
+          // onKeyDown={e => e.key === 'Enter' && onBlur(e)}
+          defaultErrorMessage={"error"}
+          helperText={"helper text"}
+        />
+      </div>
     </div>
   );
 }
